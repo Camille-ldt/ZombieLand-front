6075b5c4a1c9bc72c911d270a14640dd0814a680
LiseBARTHELEMY <lise.barthelemy@oclock.school>
2024-10-29 09:50:06 +0100

    Install Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* Ajout des chemins d'accès à tous les fichiers modèles. */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
