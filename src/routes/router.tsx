c64f54aa20836e720f758a79b44b19cffe9d80e4
Camille6313 <camille.laidet63@gmail.com>
2024-11-15 09:29:18 +0100

    Ajout de routes

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -14,84 +14,89 @@ import BackOfficeDashboard from "../Pages/BackOfficeDashboard";
 import BackOfficeReservations from "../Pages/BackOfficeReservations";
 import ProtectedRoute from "../components/ProtectedRoute";
 import UserReservation from "../Pages/Booking";
+import BackOfficeUser from "../Pages/BackOfficeUser";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <Error404 />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/activities",
-        element: <Activities />,
-      },
-      {
-        path: "/bookings",
-        element: <UserReservation />,
-      },
-      {
-        path: "/aboutus",
-        element: <HomePage />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/register",
-        element: <Register />,
-      },
-      {
-        path: "/activity",
-        element: <Activity />,
-      },
-      {
-        path: "/user/:userId",
-        element: <Profil />,
-      },
-      {
-        path: "/informations-utiles",
-        element: <UsefulInformation />,
-      },
-      // Routes administratives protégées
-      {
-        path: "/adminactivities",
-        element: (
-          <ProtectedRoute requiredRole={3}>
-            <BackOfficeActivities />
-          </ProtectedRoute>
-        ),
-        errorElement: <Error403 />, 
-      },
-      {
-        path: "/admindashboard",
-        element: (
-          <ProtectedRoute requiredRole={3}>
-            <BackOfficeDashboard />
-          </ProtectedRoute>
-        ),
-        errorElement: <Error403 />, 
-      },
-      {
-        path: "/adminreservations",
-        element: (
-          <ProtectedRoute requiredRole={3}>
-            <BackOfficeReservations />
-          </ProtectedRoute>
-        ),
-        errorElement: <Error403 />, 
-      },
-    ],
-  },
-  // 403 en accés libre pour test
-  {
-    path: "/403",
-    element: <Error403 />,
-  },
+	{
+		path: "/",
+		element: <Layout />,
+		errorElement: <Error404 />,
+		children: [
+			{
+				path: "/",
+				element: <HomePage />,
+			},
+			{
+				path: "/activities",
+				element: <Activities />,
+			},
+			{
+				path: "/bookings",
+				element: <UserReservation />,
+			},
+			{
+				path: "/aboutus",
+				element: <HomePage />,
+			},
+			{
+				path: "/login",
+				element: <Login />,
+			},
+			{
+				path: "/register",
+				element: <Register />,
+			},
+			{
+				path: "/activity",
+				element: <Activity />,
+			},
+			{
+				path: "/user/:userId",
+				element: <Profil />,
+			},
+			{
+				path: "/informations-utiles",
+				element: <UsefulInformation />,
+			},
+			// Routes administratives protégées
+			{
+				path: "/adminactivities",
+				element: (
+					<ProtectedRoute requiredRole={3}>
+						<BackOfficeActivities />
+					</ProtectedRoute>
+				),
+				errorElement: <Error403 />,
+			},
+			{
+				path: "/admindashboard",
+				element: (
+					<ProtectedRoute requiredRole={3}>
+						<BackOfficeDashboard />
+					</ProtectedRoute>
+				),
+				errorElement: <Error403 />,
+			},
+			{
+				path: "/adminreservations",
+				element: (
+					<ProtectedRoute requiredRole={3}>
+						<BackOfficeReservations />
+					</ProtectedRoute>
+				),
+				errorElement: <Error403 />,
+			},
+			{
+				path: "/adminuser",
+				element: <BackOfficeUser />,
+			},
+		],
+	},
+	// 403 en accés libre pour test
+	{
+		path: "/403",
+		element: <Error403 />,
+	},
 ]);
 
 export default router;
