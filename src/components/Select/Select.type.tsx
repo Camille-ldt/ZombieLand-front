e0e56e74cc82f44c9054b0eabe6dc5ec26ebb7ac
Camille6313 <camille.laidet63@gmail.com>
2024-11-19 10:49:16 +0100

    Ajout commentaires

--- src/components/Select/Select.type.tsx
+++ src/components/Select/Select.type.tsx
@@ -1,5 +1,5 @@
 // T c'est pour passer un type défini au moment du typage
 export interface SelectOptionItem<T> {
   name: string;
-  value: T; // type générique (Pas besoin de spécifier un type particulier)
+  value: T; // Generic type (No need to specify a particular type)
 }
